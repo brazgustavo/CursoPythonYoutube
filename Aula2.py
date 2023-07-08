41fac5bf9d975310a6f9ea06d2679bae48e1f7fe
Gustavo Braz <129451470+brazgustavo@users.noreply.github.com>
2023-07-08 16:47:38 -0300

    Add files via upload

---
+++ Aula2.py
@@ -0,0 +1,21 @@
+#len - mostra o tamanho da string
+#variavel = 'Pedro'
+#print(len(variavel))
+
+
+#lower - TRANSFORMA TUDO EM LETRA MINUSCULA
+#variavel_2 = 'PEDRO'
+#print(variavel_2.lower())
+
+#upper tranforma tudo em letra maiscula
+#variavel = 'gustavo'
+#print(variavel.upper())
+
+#strip
+#variavel = ' gustavo '
+#print(variavel.strip())
+
+#replace
+variavel = 'Gustavo'
+variavel = variavel.replace('G','O')
+print(variavel)
